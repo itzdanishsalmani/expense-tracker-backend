ffe0d29f4719f8ac8a6aa85512644ca08858a5c8
Mohammed Danish Salmani <salmanidanish488@gmail.com>
2026-08-09 20:29:25 +0530

    [FIX]: Check APi

--- index.ts
+++ index.ts
@@ -12,6 +12,16 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get("/health", (req: Request, res: Response): any => {
+
+    return res.json({
+        success: true,
+        message: "Server is running",
+        time: new Date(),
+    })
+});
+
+
 app.get("/api/check-version", (req: Request, res: Response): any => {
     const version = Number(req.query.version);
 
@@ -24,6 +34,27 @@ app.get("/api/check-version", (req: Request, res: Response): any => {
         console.log(error);
     }
 });
+
+app.post("/create-user", (req:Request, res:Response)=> {
+    const {email, password} = req.body
+
+    try {
+        // const checkEmail = await p
+        const checkEmail = false
+
+        if(checkEmail) {
+            return res.json({
+                status:400,
+                messaage:"User already exist"
+            })
+        }
+
+        // db create user with email, password
+
+    } catch (error) {
+        
+    }
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
